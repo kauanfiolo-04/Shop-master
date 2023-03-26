6e3241c5e59722eca2573969409f0ab17c6d774d
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-03-25 23:50:52 -0300

    Subindo

---
+++ frontend/src/pages/Shop/Shop.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Shop=()=>{
+  return(
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Trufas Doçuras</h1>
+      </div>
+    </div>
+  )
+}
+
+export {
+  Shop
+}
